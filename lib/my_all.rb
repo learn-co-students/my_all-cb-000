1025ebe542466df05952b03b890f304aeca50f9f
Kaveri Bhattacharya <kavericb@hotmail.com>
2016-12-03 21:17:31 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  results = []
+  i = 0
+  while i < collection.length
+    results << yield(collection[i])
+    binding.pry
+    i += 1
+  end
 
+if results.include?(false)
+  false
+else
+  true
+end
 end
