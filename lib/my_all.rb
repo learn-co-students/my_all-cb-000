25c40a9eee74439dc29b4aee0f25b6ba3a083f5e
Kyle Much <kylemuch@gmail.com>
2017-01-25 22:39:55 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
+#yield abstracts away the code block we are passing our element of the collection yielded.
