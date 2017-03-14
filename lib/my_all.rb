48e9aa820205da85465a8acd92b2e9564c525da2
youssefmys <mohamed.saleh@vet.svu.edu.eg>
2017-03-14 14:58:25 +0200

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,24 @@
-require 'pry'
+require "pry"
 
 def my_all?(collection)
 
+  if block_given?
+    i = 0 
+    block_return_values = Array.new
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i += 1
+    end
+    
+    if block_return_values.include?(false)
+      return false
+    else
+      return true
+    end
+  end
+  
+
 end
+
+
+my_all?([1,2,3]) {|i| i < 2}
