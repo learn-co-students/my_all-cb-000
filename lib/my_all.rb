395924b3d3f5686c3613797487275928ce1376d8
Matthew McAnelly <mail@matthewmcanelly.com>
2017-04-11 14:07:54 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  consistent = true
+  index = 0
+  while index < collection.size
+    if yield(collection[index])
+      index += 1
+    else
+      consistent = false
+      break
+    end
+  end
+  consistent
 end
