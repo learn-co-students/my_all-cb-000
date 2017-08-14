9710604db766d27b54792d1387402f6bb5f703cd
Jen Poole <contact.vandepool@gmail.com>
2017-08-14 02:39:21 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
+  # 1. iterate through each element in the collection, using while; increment by 1 each time
+  i = 0
 
+  while i < collection.length
+    i += 1
+  end
 end
