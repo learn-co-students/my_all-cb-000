1d9891329e8739312e1d2f9e4aa1bc963af082a4
Jen Poole <contact.vandepool@gmail.com>
2017-08-14 02:42:40 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,10 +3,10 @@ require 'pry'
 def my_all?(collection)
   # 1. iterate through each element in the collection, using while; increment by 1 each time
   i = 0
-
+  block_return_values = [] # new array
   while i < collection.length
     # 2. yield each element in the collection to the block
-    yield(collection[i])
+    block_return_values << yield(collection[i]) # 3. save the return value of the yield block (shovel return values into new array)
     i += 1
   end
 end
