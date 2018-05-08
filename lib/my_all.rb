31f3ec174e6e441f5ffa7cd87a027e3e2c3d4cc5
Phoenix <phoenix@anecso.com>
2018-05-08 22:22:55 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_value = []
+  while i < collection.size
+    block_value.push(yield collection[i])
+    i += 1
+  end
+  block_value.include?(FALSE) ? FALSE : TRUE
 end
