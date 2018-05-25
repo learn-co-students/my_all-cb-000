5c7f36be0830075ca791658b3c9023b4a34b49d9
derrick b <dbozich1@gmail.com>
2018-05-25 19:18:39 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  returned_values = []
+  while i < collection.length
+    returned_values << yield(collection[i])
+    i += 1
+
+  end
+
+  if returned_values.include?(false)
+    false
+  else
+    true
+  end
+
+end
+
+my_all?([1,2,3]) do |element|
+  element < 2
 
 end
