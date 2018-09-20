4b20d55be723032200b94a21e4fde07385687b22
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-20 10:05:27 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if block_given?
+    results = []
+    i = 0
+    while i < collection.size
+      results << yield(collection[i])
+      i += 1
+    end
+    if results.include?(false)
+      false
+    else
+      true
+    end
+  else
+    "No block was provided!"
+  end
 end
